e4e35c3b4188dfb1f1b30cfc48c6859014ef294c
way2shop <21l760@gecskp.ac.in>
2024-04-12 13:40:08 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Shopkeeperlogin(),
+      home: ShopHome(),
         ),
     );
   }
