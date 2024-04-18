fceecea940e4a63969124ef6a9fdd51840c3466b
HIBA RASHID <am5291667@gmail.com>
2024-04-18 16:59:27 +0530

    hiba

--- lib/Model/ItemModel.dart
+++ lib/Model/ItemModel.dart
@@ -1,9 +1,23 @@
 
 class ItemModel {
-  String name;
-  String code;
-  String quantity;
-  ItemModel(this.name,this.code,this.quantity);
+  String itemid;
+  String photo;
+  String itemname;
+  String price;
+  String category;
+  String categoryid;
+  String description;
+  String itemquartity;
+  String offers;
+  String color;
+  String brand;
+  String productdiemension;
+  String assmbly;
+  String productcare;
+  String instruction;
+
+  ItemModel(this.itemid,this.photo,this.itemname,this.price,this.category,this.categoryid,this.description,this.itemquartity,this.offers
+      ,this.color,this.brand,this.productdiemension,this.assmbly,this.productcare,this.instruction);
 
 }
 
